d6b0cb63ddcd978ac4f64b120e6c446da8fbefc4
g.racic <g.racic@criteo.com>
2022-03-16 17:02:21 +0100

    Clean up TensorFlow module for Spark inference

--- ml_hadoop_experiment/tensorflow/dataframe_prediction_helper.py
+++ ml_hadoop_experiment/tensorflow/dataframe_prediction_helper.py
@@ -3,7 +3,8 @@ from typing import (
     List,
     Union,
     Callable,
-    Tuple
+    Tuple,
+    Any
 )
 
 import tensorflow as tf
@@ -26,74 +27,60 @@ features_specs_type = Dict[
             tf.io.VarLenFeature
         ]
     ]
-extract_fn_type = Callable[[Dict[str, np.ndarray]], pd.Series]
+postprocessing_fn_type = Callable[[Dict[str, np.ndarray]], pd.Series]
 estimator_type = Callable[[List[Dict]], List[Union[float, List[float]]]]
-
 _default_signature = tf.compat.v1.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
 
-# User-defined function to compute inference with PyTorch models
+# User-defined function to compute inference with TensorFlow models
 # Inputs: artifacts, list of feature columns
 # Output: inference columnn
 inference_udf = Callable[[artifact_type, Tuple[pd.Series, ...]], pd.Series]
-
 
 def _canned_linear_classifier_extract_prediction_fn(
     fetch_tensors: Dict[str, List]
 ) -> List[float]:
     '''
-    fetch_tensors: inference result with canned LinearClassifier estimator.
-        The result is a dictionary mapping keyword 'scores' to a list of tuples,
-        as many tuples as examples given to the estimator for inference.
-        Each tuple consists of two probabilities, one per class
+    fetch_tensors: output of canned LinearClassifier estimator:
+        {
+            'scores': [
+                (proba_class1_example1, proba_class2_example1, proba_class3_example1, ...),
+                (proba_class1_example2, proba_class2_example2, proba_class3_example2, ...),
+                (proba_class1_example3, proba_class2_example3, proba_class3_example3, ...),
+                ...
+            ]
+        }
 
-    Return: List of predictions of positive class, as many as examples given to
-        the estimator for inference.
+    Return: probabilities of class 1 (which is the positive class for binary classfiers)
     '''
     return [float(s[1]) for s in fetch_tensors['scores']]
 
 
-def predict_with_tfr(
-    features_specs: features_specs_type,
-    model_path: str,
-    extract_prediction_fn: Callable = _canned_linear_classifier_extract_prediction_fn,
-    feed_tensor_key: str = "inputs"
-) -> estimator_type:
-    estimator = tf.compat.v1.saved_model.load_v2(model_path)
+def _default_extract_fn(fetch_tensors: Dict[str, np.ndarray]) -> pd.Series:
+    '''
+    Extract probabilities of the first class, which corresponds to the positive class
+    for binary classifiers.
 
-    def _predict(
-        inputs: List[Dict]
-    ) -> List[Union[float, List[float]]]:
-        import tensorflow as tf
-        if not tf.executing_eagerly():
-            tf.compat.v1.enable_eager_execution()
-        serialized_tfrecords = [
-            tfrecords.to_tf_proto(
-                e,
-                features_specs
-            ).SerializeToString()
-            for e in inputs]
-        results = estimator.signatures[_default_signature](
-            **{feed_tensor_key: tf.constant(serialized_tfrecords)})
-        return extract_prediction_fn(results)
-    return _predict
+    fetch_tensors:
+        {
+            'scores': [
+                (proba_class1_example1, proba_class2_example1, proba_class3_example1, ...),
+                (proba_class1_example2, proba_class2_example2, proba_class3_example2, ...),
+                (proba_class1_example3, proba_class2_example3, proba_class3_example3, ...),
+                ...
+            ]
+        }
 
-
-def default_extract_fn(outputs: Dict[str, np.ndarray]) -> pd.Series:
-    return pd.Series(outputs["scores"][:, 1])
-
-
-def filtered_columns(
-    df: pyspark.sql.dataframe.DataFrame,
-    specs: features_specs_type
-) -> List[Column]:
-    return [df[x] for x in df.columns if x in specs.keys()]
+    Return: probabilities of class 1
+    '''
+    return pd.Series(fetch_tensors["scores"][:, 1])
 
 
 def estimator_model(
     sparkSession: pyspark.sql.SparkSession, export_model_path: str
 ) -> SerializableObj:
     """
-    Wrap an model built with Estimator API for inference with Spark
+    Wrap a model built with Estimator API for inference with Spark
+    Wrapped model is guaranteed to be broadcastable
     """
     return SerializableObj(sparkSession, tf.compat.v1.saved_model.load_v2, export_model_path)
 
@@ -103,6 +90,7 @@ def keras_model(
 ) -> SerializableObj:
     """
     Wrap a Keras model for inference with Spark
+    Wrapped model is guaranteed to be broadcastable
     """
     return SerializableObj(sparkSession, tf.keras.models.load_model, export_model_path)
 
@@ -113,6 +101,7 @@ def graph_model(
 ) -> SerializableObj:
     """
     Wrap a graph model for inference with Spark
+    Wrapped model is guaranteed to be broadcastable
     """
     return SerializableObj(
         sparkSession, Predictor.from_graph, export_model_path, feeds, fetches)  # type: ignore
@@ -120,24 +109,27 @@ def graph_model(
 
 def with_graph_inference_column(
     df: pyspark.sql.dataframe.DataFrame,
-    predictor_model: SerializableObj,
+    model: SerializableObj,
     output_column_name: str = "prediction",
-    output_type: pyspark.sql.types.DataType = FloatType(),
-    extract_fn: extract_fn_type = lambda x: pd.Series(x["score"][:, 0])
+    output_column_type: pyspark.sql.types.DataType = FloatType(),
+    postprocessing_fn: postprocessing_fn_type = lambda x: pd.Series(x["score"][:, 0])
 ) -> pyspark.sql.dataframe.DataFrame:
     """
-    Add a column in the dataframe that is the result of the inference of a model.
+    Runs inference on the input dataframe and adds a column 'output_column_name' with
+    your postprocessed model outputs.
+    Method to use if your model is a graph.
 
     :param df: the dataframe to add a prediction to
-    :param predictor_model: instance of SerializableObj
+    :param model: instance of SerializableObj wrapping your model
     :param output_column_name: name of the newly-created inference column
-    :param output_type: output type of the predictor model
-    :param extract_fn: method to extract the result from the estimator output dictionary and
-    transform it as a float
-    :return: a new dataframe with a column named 'column_named' that is the prediction value
+    :param output_column_type: type of the newly-created inference column
+    postprocessing_fn: postprocessing function called on your model outputs
+    The primary purpose of this functon is to extract the relevant scores/predictions of 
+    your model outputs but it is not limited to this use case.
+    :return: a new dataframe with a new column 'output_column_name'
     """
 
-    feature_names = list(predictor_model.ew.obj.feed_tensors.keys())
+    feature_names = list(model.ew.obj.feed_tensors.keys())
     for feature_name in feature_names:
         if feature_name not in df.columns:
             raise ValueError(f"{feature_name} not found in columns {df.columns}")
@@ -160,74 +152,76 @@ def with_graph_inference_column(
             }).batch(batch_size)
 
         outputs = next(model.predict(input_fn))
-        return extract_fn(outputs)
+        return postprocessing_fn(outputs)
 
     return with_inference(
-        df, predictor_model, feature_names, _inference_fn, output_type,
-        output_col=output_column_name
+        df, model, _inference_fn, feature_names, output_column_type, output_column_name
     )
 
 
 def with_inference_column(
     df: pyspark.sql.dataframe.DataFrame,
     tfrecords_col: Union[pyspark.sql.Column, str],
-    estimator_model: artifact_type,
-    column_name: str = "prediction",
-    input_key: str = "inputs",
-    extract_fn: extract_fn_type = default_extract_fn
+    model: artifact_type,
+    output_column_name: str = "prediction",
+    feed_tensor_key: str = "inputs",
+    postprocessing_fn: postprocessing_fn_type = _default_extract_fn
 ) -> pyspark.sql.dataframe.DataFrame:
     """
-    Add a column in the dataframe that is the result of the inference of a model.
-    This is computed line-by-line with a UDF.
-    The estimator is required to take as input a TFR example
+    Runs inference on the input dataframe and adds a column 'output_column_name' with
+    your postprocessed model outputs.
+    Method to use if your model has been built with the Estimator API
 
     :param df: the dataframe to add a prediction to
-    :param tfrecords_col: column containing your model inputs in tfrecord format
-    :param estimator_model: instance of SerializableObj
-    :param column_name: name of the newly-created inference column
-    :param input_key: key where the estimator function is expecting to find the TFR record
-    :param extract_fn: method to extract the result from the estimator output dictionary and
-    transform it as a float
-    :return: a new dataframe with a column named 'column_named' that is the prediction value
+    :paramm tfrecords_col: column of your dataframe containing the tfrecords to use as inputs
+    of your model
+    :param model: model to use for inference
+    :param output_column_name: name of the newly-created inference column
+    :param feed_tensor_key: feed tensor key to use to feed your model with inputs
+    postprocessing_fn: postprocessing function called on your model outputs
+    The primary purpose of this functon is to extract the relevant scores/predictions of 
+    your model outputs but it is not limited to this use case.
+    :return: a new dataframe with a new column 'output_column_name'
     """
     def _inference_fn(model: artifact_type, series: Tuple[pd.Series, ...]) -> pd.Series:
         import tensorflow as tf
         if not tf.executing_eagerly():
             tf.compat.v1.enable_eager_execution()
         outputs = model.signatures[_default_signature](
-            **{input_key: tf.constant(series[0])}
+            **{feed_tensor_key: tf.constant(series[0])}
         )
-        return extract_fn(outputs)
+        return postprocessing_fn(outputs)
 
     return with_inference(
-        df, estimator_model, [tfrecords_col], _inference_fn, FloatType(),
-        output_col=column_name
+        df, model, _inference_fn, [tfrecords_col], FloatType(), output_column_name
     )
 
 
 def with_inference(
     df: pyspark.sql.DataFrame,
     model: artifact_type,
-    input_cols: List[Union[str, pyspark.sql.Column]],
     inference_fn: inference_udf,
-    output_type: DataType,
+    input_column_names: List[Union[str, pyspark.sql.Column]],
+    output_column_type: DataType,
+    output_column_name: str = "prediction",
     batch_size: int = 1,
-    output_col: str = "prediction",
     num_threads: int = 8,
 ) -> pyspark.sql.dataframe.DataFrame:
     """
-    :param ss: Spark sesson used to create the dataframe
+    Runs 'inference_fn' on the input dataframe and adds a column 'output_column_name' with
+    outputs of 'inference_fn'
+
     :param df: dataframe that holds the input
     :param model: model to use for inference
-    :param input_col: names of the dataframe columns that will be used as inputs for inference
-    :param inference_fn: function that is run to compute predictons.
+    :param inference_fn: function run for inference.
     It takes as inputs a model and a list of pandas series. Each pandas serie represent
-    one of the dataframe column of :param input_cols, in the same order.
-    It returns a Pandas Serie
-    :param output_type: output type of the predictions
-    :param batch_size: batch size
-    :param output_col: name of the colonne of the dataframe that will hold the predictions
-    :param num_threads: Number of threads to run inter/inta-ops
+    one of the dataframe column of :param input_column_names, in the same order.
+    It returns a Pandas Serie containing outputs of 'inference_fn'
+    :param input_column_names: columns used for inference
+    :param output_column_type: type of the newly-created inference column
+    :param output_column_name: name of the newly-created inference column
+    :param batch_size: batch size used to during inference
+    :param num_threads: Number of threads for inter/inta-ops used during inference
     """
     broadcasted_artifacts = broadcast(df._sc, model)
 
@@ -246,12 +240,54 @@ def with_inference(
         results = pd.Series()
         for batch in split_in_batches(rows, batch_size):
             results = results.append(inference_fn(artifacts, batch))
-        print(f"FINAL RESULT: {results}")
         return results
 
-    inference_udf = sf.pandas_udf(_inference_fn, returnType=output_type)
+    inference_udf = sf.pandas_udf(_inference_fn, returnType=output_column_type)
     # In some situation, the pandas udf can be computed more than once when the
     # output column is referenced more than once,
     # (https://issues.apache.org/jira/browse/SPARK-17728).
     # One workaround is to wrap the pandas udf in sf.explode(sf.array())
-    return df.withColumn(output_col, sf.explode(sf.array(inference_udf(*input_cols))))
+    return df.withColumn(
+        output_column_name,
+        sf.explode(sf.array(inference_udf(*input_column_names)))
+    )
+
+
+def predict_with_tfr(
+    features_specs: features_specs_type,
+    model_path: str,
+    postprocessing_fn: Callable = _canned_linear_classifier_extract_prediction_fn,
+    feed_tensor_key: str = "inputs"
+) -> estimator_type:
+    '''
+    features_specs: specifications of your model input features
+    model_path: path to your model
+    postprocessing_fn: postprocessing function called on your model outputs
+    The primary purpose of this functon is to extract the relevant scores/predictions of 
+    your model outputs but it is not limited to this use case.
+    feed_tensor_key: feed tensor key to feed your model with inputs
+    '''
+    estimator = tf.compat.v1.saved_model.load_v2(model_path)
+
+    def _predict(
+        inputs: List[Dict]
+    ) -> List[Any]:
+        if not tf.executing_eagerly():
+            tf.compat.v1.enable_eager_execution()
+        serialized_tfrecords = [
+            tfrecords.to_tf_proto(
+                e,
+                features_specs
+            ).SerializeToString()
+            for e in inputs]
+        results = estimator.signatures[_default_signature](
+            **{feed_tensor_key: tf.constant(serialized_tfrecords)})
+        return postprocessing_fn(results)
+    return _predict
+
+
+def filtered_columns(
+    df: pyspark.sql.dataframe.DataFrame,
+    specs: features_specs_type
+) -> List[Column]:
+    return [df[x] for x in df.columns if x in specs.keys()]
