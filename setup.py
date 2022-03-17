57fd208e036a187e3bbedad0c4dea6bfd423c3c8
g.racic <g.racic@criteo.com>
2022-03-17 13:35:57 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     name="ml_hadoop_experiment",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    version="0.0.1",
+    version="0.0.2",
     install_requires=REQUIREMENTS,
     tests_require=_read_reqs("tests-requirements.txt"),
     python_requires=">=3.6",
